e7e42db0e5a37ee91a8847183a94fb754d102606
ReeceLangerock <ReeceLangerock@gmail.com>
2017-08-08 22:37:08 -0500

    add basic component tests

---
+++ src/tests/portfolio.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Portfolio from './components/portfolio/Portfolio'
+
+it('Portfolio renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Portfolio />, div);
+});
