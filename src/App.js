f6e2671c2db9f28b55919d2e72630f2bbdfabbb1
ReeceLangerock <ReeceLangerock@gmail.com>
2017-08-08 22:39:10 -0500

    add updated package and app/index files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+
+//import components
+import Navigation from './components/navigation/Navigation';
+import Hero from './components/hero/Hero';
+import Footer from './components/footer/Footer';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Navigation/>
+        <section id = "section__hero">
+        <Hero/>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+          <h1> TEST</h1>
+        </section>
+        <section id = "section__footer">
+        <Footer/>
+
+        <div id ="test"><h1>testerrrr</h1></div>
+          </section >
+          <div></div>
       </div>
+      </BrowserRouter>
     );
   }
 }
