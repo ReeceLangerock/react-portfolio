f237ee034f0ed8454ecd9fa9e870937e69c7303b
ReeceLangerock <ReeceLangerock@gmail.com>
2017-08-20 16:35:18 -0500

    add nav item and additinal info to about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./../../style/about.css";
+var smoothScroll = require("smoothscroll");
 
 export class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  scrollToContact() {
+    let contact = document.getElementById('contact');
+    smoothScroll(contact,1000)
+  }
   render() {
     return (
       <section id="about">
@@ -11,9 +19,10 @@ export class About extends React.Component {
               <span className="about__text-hi">
                 hi! <br />
               </span>
-              <span className="about__text-main">my name is <br /></span>
+              <span className="about__text-main">
+                my name is <br />
+              </span>
               <span className="about__text-emphasis">
-              
                 <b>reece</b>
                 <br />
               </span>
@@ -22,11 +31,15 @@ export class About extends React.Component {
                 <br /> front-end developer<br />
               </span>{" "}
               <span className="about__text-main">
-                take a look around and let me <a>know</a> if you have any questions!
+                take a look around and let me know below{" "}
+             
+                  <i className="fa fa-arrow-down toBottom" aria-hidden="false" onClick={this.scrollToContact}/>
+                {" "}
+                if you have any questions!
               </span>
             </div>
             <div className="about__image">
-              <img alt = "about-me" src={require("./../../assets/images/temp.ico")} />
+              <img alt="about-me" src={require("./../../assets/images/temp.ico")} />
             </div>
           </div>
         </div>
